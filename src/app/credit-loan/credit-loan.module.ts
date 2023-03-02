e08d146118d95f5e46eeace920ccfc61090dc8cd
horusen <babacarkane@meltwater.org>
2023-03-02 16:32:47 +0000

    Credit loan configuration

--- src/app/credit-loan/credit-loan.module.ts
+++ src/app/credit-loan/credit-loan.module.ts
@@ -1,10 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreditLoanComponent } from './credit-loan.component';
 import { CreditLoanCreateComponent } from './credit-loan-create/credit-loan-create.component';
+import { SharedModule } from '../shared/shared.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CreditLoanComponent,
+  },
+];
 
 @NgModule({
   declarations: [CreditLoanComponent, CreditLoanCreateComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
 })
 export class CreditLoanModule {}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,24 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'credit-loan',
+    loadChildren: () =>
+      import('../credit-loan/credit-loan.module').then(
+        (module) => module.CreditLoanModule
+      ),
+  },
+];
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class DashboardModule { }
+export class DashboardModule {}
