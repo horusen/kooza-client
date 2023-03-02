715b01edf0a18667c223375c8dd5087678a81986
Rainer Mbongo <rainermbongo@gmail.com>
2023-03-02 11:41:14 +0000

    push

---
+++ src/app/retailers/retailers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retailers',
+  templateUrl: './retailers.component.html',
+  styleUrls: ['./retailers.component.scss']
+})
+export class RetailersComponent {
+
+}
